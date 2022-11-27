c3eec22eb5f2081a5642096568db670aff1f68fb
Paul Yuknewicz <paulyuk@Pauls-MBP-2.lan>
2022-11-27 01:32:28 -0800

    Replicating community user scenario with node.js code
    Signed-off-by: Paul Yuknewicz <paulyuk@Pauls-MBP-2.lan>

---
+++ testscript1/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const fs = require('fs').promises;
+
+  const data = await fs.readFile('../resources/textdata.txt', 'utf8');
+
+  const responseMessage = data.toString();
+  console.log("Text file response: " + data);
+
+  res.end(responseMessage);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
