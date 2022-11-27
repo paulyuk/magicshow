a11b87fa34c8ad6115a3c84383d0bf99dd7bded1
Paul Yuknewicz <paulyuk@Pauls-MBP-2.lan>
2022-11-27 01:50:13 -0800

    refactored reusable fileread function out
    Signed-off-by: Paul Yuknewicz <paulyuk@Pauls-MBP-2.lan>

--- testscript1/app.js
+++ testscript1/app.js
@@ -7,15 +7,22 @@ const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
-  const fs = require('fs').promises;
-
-  const data = await fs.readFile('../resources/textdata.txt', 'utf8');
+  const data = await readTextData();
 
   const responseMessage = data.toString();
   console.log("Text file response: " + data);
 
   res.end(responseMessage);
 });
+
+async function readTextData() {
+  const fs = require('fs').promises;
+
+  const data = await fs.readFile('../resources/textdata.txt', 'utf8')
+    .catch((err) => console.error('Failed to read file', err));
+
+  return data;
+}
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
